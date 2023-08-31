61481abe608d0312714d86858013798d03e9e9b6
Kush-munot <munot.kgm@gmail.com>
2023-08-31 22:57:54 +0530

    :zap: Widget Nesting Done !!

--- first_demo_app/lib/main.dart
+++ first_demo_app/lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: Text('Hello Kush !! Welcome to Netrin Sports tecnology')));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.purple, Colors.blue]),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello Kush !! Welcome to Netrin Sports technology',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
