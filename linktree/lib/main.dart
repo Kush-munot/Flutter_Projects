02365d4615aa19ba20e3b0641a96ead597c0da01
Kush-munot <munot.kgm@gmail.com>
2023-09-06 14:56:10 +0530

    :zap: Reduced Code and used component reusing

--- linktree/lib/main.dart
+++ linktree/lib/main.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+const githubUrl = 'https://github.com/Kush-munot';
+const portfolioUrl = 'https://kushmunot.netlify.app/';
+const leetcodeUrl = 'https://leetcode.com/user4382M/';
+const gfgUrl = 'https://auth.geeksforgeeks.org/user/munotkgm/practice';
+const linkedinUrl = 'https://www.linkedin.com/in/kush-munot/';
+const twitterUrl = 'https://twitter.com/intent/follow?screen_name=kushjain_17';
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: HeaderContainer(),
+        //body: HeaderContainer(),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            HeaderContainer(),
+            SizedBox(height: 16.0),
+            CustomButton(
+              githubUrl,
+              'Explore My Github',
+              Colors.black,
+              FaIcon(
+                FontAwesomeIcons.github,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            CustomButton(
+                portfolioUrl,
+                'Explore My Portfolio',
+                Colors.redAccent,
+                FaIcon(
+                  FontAwesomeIcons.faceSmileWink,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            SizedBox(height: 16.0),
+            CustomButton(
+                leetcodeUrl,
+                'Explore My LeetCode Profile',
+                Colors.orangeAccent,
+                FaIcon(
+                  FontAwesomeIcons.terminal,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            SizedBox(height: 16.0),
+            CustomButton(
+                gfgUrl,
+                'Explore My GFG Profile',
+                Colors.green,
+                FaIcon(
+                  FontAwesomeIcons.code,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            SizedBox(height: 16.0),
+            CustomButton(
+                linkedinUrl,
+                'Explore My LinkedIn',
+                Colors.blue,
+                FaIcon(
+                  FontAwesomeIcons.linkedin,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            SizedBox(height: 16.0),
+            CustomButton(
+                twitterUrl,
+                'Explore My Twitter',
+                Colors.blueGrey,
+                FaIcon(
+                  FontAwesomeIcons.twitter,
+                  color: Colors.white,
+                  size: 30,
+                )),
+            SizedBox(height: 16.0),
+          ]),
+        ),
       ),
     ),
   );
@@ -19,7 +93,7 @@ class HeaderContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 470.0,
+      height: 270.0,
       decoration: const BoxDecoration(
         color: Colors.greenAccent,
         borderRadius: BorderRadius.only(
@@ -34,17 +108,12 @@ class HeaderContainer extends StatelessWidget {
           Text(
             'Kush Munot',
             style: GoogleFonts.courgette(
-              // Replace with the name of your chosen Google Font
               textStyle: const TextStyle(
                 fontSize: 40.0, // Adjust the font size
                 color: Colors.black,
               ),
             ),
           ),
-          const SizedBox(height: 16.0), // Add spacing between the buttons
-          const GithubButton(),
-          const SizedBox(height: 16.0), // Add spacing between the buttons
-          const PortfolioButton(),
         ],
       ),
     );
@@ -67,11 +136,16 @@ class ProfileImage extends StatelessWidget {
   }
 }
 
-class GithubButton extends StatelessWidget {
-  const GithubButton({Key? key}) : super(key: key);
+class CustomButton extends StatelessWidget {
+  final String link;
+  final String text;
+  final Color btnColor;
+  final FaIcon iconData; // Icon data for the icon
+  const CustomButton(this.link, this.text, this.btnColor, this.iconData,
+      {super.key});
 
   void _launchGithubURL() async {
-    final Uri url = Uri.parse('https://github.com/Kush-munot');
+    final Uri url = Uri.parse(link);
     if (await canLaunchUrl(url)) {
       await launchUrl(url);
     } else {
@@ -85,57 +159,24 @@ class GithubButton extends StatelessWidget {
       onTap: _launchGithubURL,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.black,
+          color: btnColor,
           borderRadius: BorderRadius.circular(8.0),
         ),
         padding: const EdgeInsets.all(10.0),
-        child: const Row(
+        width: 320,
+        alignment: Alignment.center,
+        child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            SizedBox(width: 8.0),
+            iconData,
+            const SizedBox(width: 8.0),
             Text(
-              'Explore my Github',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PortfolioButton extends StatelessWidget {
-  const PortfolioButton({Key? key}) : super(key: key);
-
-  void _launchPortfolioURL() async {
-    final Uri url = Uri.parse('https://kushmunot.netlify.app/');
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: _launchPortfolioURL,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.redAccent,
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        padding: const EdgeInsets.all(10.0),
-        child: const Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(width: 8.0),
-            Text(
-              'Explore my Portfolio',
-              style: TextStyle(
-                color: Colors.white,
+              text,
+              style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
               ),
             ),
           ],
